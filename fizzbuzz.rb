d2ed95f65bc8663dd16988d1afc12034afe32768
Logan Wohlers <lawohlers@gmail.com>
2019-02-03 19:57:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (int)
-  if(int%3==0 && int%5==0)
+def fizzbuzz (num)
+  if(num%3==0 && num%5==0)
     return "FizzBuzz"
- elseif
-
+  elseif (num%3==0)
+    return "Fizz"
+  else
+    return 
+    
     
 end
