13f4589f0b40b6dc1481e23fddd189f0ff4f9a61
Logan Wohlers <lawohlers@gmail.com>
2019-02-03 20:01:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz (num)
   if num%3==0 && num%5==0
     return "FizzBuzz"
-  elseif num%3==0
+  elsif num%3==0
     return "Fizz"
-  elseif num%5==0
+  elsif num%5==0
     return "Buzz"
   else
     return nil
